887f940db8915abd595b23cfca3bc15feee54f4f
Jay-Wang-zechong <66953752+Jay-Wang-zechong@users.noreply.github.com>
2020-06-15 20:48:12 +0800

    Add files via upload

---
+++ python抽签/小组抽签.py
@@ -0,0 +1,6 @@
+import random
+
+people_num = 7
+number = random.randint(1,people_num)
+print(number)
+input("Press<Enter>")
---
+++ python抽签/抽签.py
@@ -0,0 +1,9 @@
+import random
+
+people_num = 7
+number = random.randint(1,people_num)
+print(number)
+people_num = 6
+number = random.randint(1,people_num)
+print(number)
+input("Press <Enter>")
