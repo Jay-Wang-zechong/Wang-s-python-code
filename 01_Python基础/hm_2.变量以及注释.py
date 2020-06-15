0aefa410c9221671b7bf3f76464380f6f48719d7
Jay-Wang-zechong <66953752+Jay-Wang-zechong@users.noreply.github.com>
2020-06-15 20:49:15 +0800

    Add files via upload

---
+++ 01_Python基础/hm_2.变量以及注释.py
@@ -0,0 +1,9 @@
+# 1. 定义一个变量
+qq_number ="17303668355"
+# 2.定义一个变量
+qq_password ="20080513wW"
+#想要输出得用到print函数
+qq_number
+#输出函数
+print (qq_number)
+print (qq_password)
---
+++ 01_Python基础/圆柱计算体积.py
@@ -0,0 +1,8 @@
+radius_str = input("请输入圆柱体的半径：")
+height_str = input("请输入圆柱体的高度：")
+radius = float (radius_str)
+height = float (height_str)
+pi = 3.14
+volume = radius * radius * height * pi
+print(volume)
+input("Press<Enter>")
